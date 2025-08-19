d6f5714c5fe883aeed64d9515aec21326be59cee
Sultan Faqih <=>
2025-08-19 20:54:55 +0700

    feat(gif): - update gif function

--- stream.js
+++ stream.js
@@ -207,6 +207,8 @@ app.post('/gif', (req, res) => {
     '-i', pattern,
     // '-vf', 'scale=trunc(iw/2)*2:trunc(ih/2)*2',
     '-c:v', 'libx264',
+    '-profile:v', 'baseline',
+    '-level', '3.0',
     '-pix_fmt', 'yuv420p',
     '-movflags', '+faststart',
     '-preset', 'veryfast',
