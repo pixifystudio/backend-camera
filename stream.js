2dee4e8a7b66414b202bfaec888f661037345f8c
Sultan Faqih <=>
2025-08-23 16:51:25 +0700

    feat(gif): - update gif  with orignial

--- stream.js
+++ stream.js
@@ -218,13 +218,14 @@ app.post('/gif', (req, res) => {
   const loopVideo = [
     '-stream_loop', '2',
     '-i', outputFile,
-    '-c:v', 'libx264',
-    '-profile:v', 'baseline',
-    '-level', '3.0',
-    '-pix_fmt', 'yuv420p',
-    '-movflags', '+faststart',
-    '-preset', 'veryfast',    
-    '-crf', '28',
+    '-c', 'copy',
+    // '-c:v', 'libx264',
+    // '-profile:v', 'baseline',
+    // '-level', '3.0',
+    // '-pix_fmt', 'yuv420p',
+    // '-movflags', '+faststart',
+    // '-preset', 'veryfast',    
+    // '-crf', '28',
     outputFinalFile
   ];
 
